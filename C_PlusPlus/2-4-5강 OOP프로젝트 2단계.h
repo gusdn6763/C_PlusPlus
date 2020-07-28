f6b53d1143a1201272269b352c15ff11e00569f7
gusdn6763 <gusdn6763@gmail.com>
2020-07-28 16:48:30 +0900

    Class Bank, Class People Complete
    2020-07-28
    Bank 와 People 클래스 완성,
    캐릭터 선택창은 후에...

--- C_PlusPlus/2-4-5강 OOP프로젝트 2단계.h
+++ C_PlusPlus/2-4-5강 OOP프로젝트 2단계.h
@@ -28,6 +28,8 @@ namespace Simulate
 		}
 		string		name;
 		void		GetInBank(Bank *bank);
+		void		Working();
+		void		ShowMyInfo();
 	private:
 		int			currentMoney;
 	};
@@ -39,15 +41,17 @@ namespace Simulate
 		{
 			account = new unordered_map<string, Account>;
 		}
-		void		ShowInfo(People *currentPeople);
-		int			AddMoney(People *currentPeople);
-		int			MinusMoney(People *currentPeople);
 		void		MakeAccount(People *currentPeople);
+		bool		FindAccount(People* currentPeople);
+		int			CheckMoney(int money);
+		int			DepositAndWithdrawal(People *currentPeople, bool moneyComp, int &money);
+		bool		CheckPassword(People* currentPeople);
+		void		ShowInfo(People *currentPeople);
 	private:
 		unordered_map<string, Account> *account;
 	};
 
 	void	ShowChooseNum(const char* str);
 	int		main245();
-	string GetName();
+	string	GetName();
 }
